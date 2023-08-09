fa402aeb3b87b7f39c9123b910c6625d9888bf58
aabedraba <aabedraba@gmail.com>
2023-08-09 19:39:08 +0400

    group in folderS

---
+++ modules/handlers/stripe/has-active-subscription.ts
@@ -0,0 +1,26 @@
+import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+import { getUserInfo } from "../../utils/user-info";
+import { getStripeSubscriptionByEmail } from "../../services/stripe";
+import { ErrorResponse } from "../../types";
+
+export default async function stripeActiveSubscription(
+  request: ZuploRequest,
+  context: ZuploContext
+) {
+  const userInfo = await getUserInfo(request, context);
+
+  if (userInfo instanceof ErrorResponse) {
+    return userInfo;
+  }
+
+  const stripeSubscription = await getStripeSubscriptionByEmail({
+    customerEmail: userInfo.email,
+    logger: context.log,
+  });
+
+  if (stripeSubscription instanceof ErrorResponse) {
+    return stripeSubscription;
+  }
+
+  return request;
+}
